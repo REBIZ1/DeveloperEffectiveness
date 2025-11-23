3e3500ffd433f1201bfedab6c8e8df032233294d
Arthur <arthur155188@gmail.com>
2025-11-23 21:42:55 +0500

    Изменил функцию process_files, теперь она возвращает список словарей

--- main/file_handler.py
+++ main/file_handler.py
@@ -1,25 +1,21 @@
 import csv
 
-def process_files(*args):
+def process_files(files: list[str]) -> list[dict[str, str]]:
     """
-    Рассчитывает среднюю эффективность по позициям
-    Принимает csv файлы
-    Возвращает отсортированный список кортежей
+    Обрабатывает файлы и возвращает список словарей
     """
-    data = {}
+    rows = []
 
-    for file in args:
+    for file in files:
         try:
+            if not file.endswith('.csv'):
+                raise ValueError('Поддерживаются только .csv файлы!')
             with open(file, 'r', encoding='utf-8') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    data.setdefault(row['position'], []).append(float(row['performance']))
-        except FileNotFoundError as e:
-            print(f'Ошибка, файл "{file}" не найден!: {e}')
-            return None
-
-    # вычисляем среднее значение по позиции
-    data = {k: round(sum(v) / len(v), 2) for k, v in data.items()}
-    # сортируем и возвращаем список кортежей
-    data = sorted(data.items(), key=lambda x: x[1], reverse=True)
-    return data
+                    rows.append(row)
+        except FileNotFoundError:
+            raise FileNotFoundError(f'Файл "{file}" не найден')
+        except ValueError as e:
+            raise ValueError(f'Ошибка при обработке файла {file}: {e}')
+    return rows
